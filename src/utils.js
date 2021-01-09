40f3e754e731f6135ae7b13bfb5a767574172865
Vladimir <vladimirleontiev.devmail@gmail.com>
2021-01-09 22:05:10 +0300

    commit__2021-01-09__22-05

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const getNewsImagePath = (item, options) => {
   let size = (options && options.size) ? options.size : null;
-  let defaultValue = '/img/defaultImage.jpg';
+  let defaultValue = './img/defaultImage.jpg';
   let value = (size === 'large') ? item.images.large : item.images.small;
   let alternative = (size === 'large') ? item.images.small : item.images.large;
   return (value) ? value : (alternative) ? alternative : defaultValue;
